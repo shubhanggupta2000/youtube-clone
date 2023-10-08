75fa6fe5f78b4084c4f16d8d672e35522dc4a87e
Shubhang Gupta <guptashubhang2000@gmail.com>
2023-10-09 00:48:06 +0530

    refresh

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Youtube Clone
-A simple video streaming clone of youtube built with react using Youtube-v3 API.
 
-The app's Interface has been made with the help of Material UI (version 5). Data used in here has been obtained from Rapid API.
+- A simple video streaming clone of youtube built with ReactJS using Youtube-v3 API.
 
-Click [here](https://fazevennom-youtubeclone.netlify.app/) for the Hostlink and experience the web-app.
+- The app's Interface has been made with the help of Material UI (version 5). Data used in here has been obtained from Rapid API.
+
+- Click [here](https://fazevennom-youtubeclone.netlify.app/) for the Hostlink and experience the web-app.
 
 # Glimpse
+
 ![Screenshot from 2022-10-09 17-39-20](https://user-images.githubusercontent.com/79959361/194756175-306d72fe-7df2-4dde-a693-cc4c07973783.png)
 
 ![Screenshot from 2022-10-09 17-39-52](https://user-images.githubusercontent.com/79959361/194756179-b3013bb5-493c-4063-8bcd-0ccb3c170e0a.png)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,43 +1,45 @@
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
-import SchoolIcon from '@mui/icons-material/School';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
-import Engineering from '@mui/icons-material/Engineering';
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import Engineering from "@mui/icons-material/Engineering";
 
-export const logo = 'https://i.ibb.co/n1GHSm3/youtube.png';
+export const logo = "https://i.ibb.co/n1GHSm3/youtube.png";
 
 export const categories = [
-  { name: 'New', icon: <HomeIcon />, },
-  { name: 'Priyanshu [IIIT-G]', icon: <Engineering />, },
-  { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
-  { name: 'NextJS', icon: <CodeIcon />, },
-  { name: 'Music', icon: <MusicNoteIcon /> },
-  { name: 'Education', icon: <SchoolIcon />, },
-  { name: 'Podcast', icon: <GraphicEqIcon />, },
-  { name: 'Movie', icon: <OndemandVideoIcon />, },
-  { name: 'Gaming', icon: <SportsEsportsIcon />, },
-  { name: 'Live', icon: <LiveTvIcon />, },
-  { name: 'Sport', icon: <FitnessCenterIcon />, },
-  { name: 'Fashion', icon: <CheckroomIcon />, },
-  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
-  { name: 'Comedy', icon: <TheaterComedyIcon />, },
-  { name: 'Gym', icon: <FitnessCenterIcon />, },
-  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Priyanshu [IIIT-G]", icon: <Engineering /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/c3WBK2t/thumbnail.jpg';
-export const demoChannelUrl = '/channel/UCwBjdts6grkMXpVFebk60ZQ';
-export const demoVideoUrl = '/video/YNZ7dhPH6P0';
-export const demoChannelTitle = 'Priyanshu [IIIT-G]';
-export const demoVideoTitle = '30 Days in 1 Minute | IIIT GWALIOR | Hostel Life Mini vlog';
-export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
+export const demoThumbnailUrl = "https://i.ibb.co/c3WBK2t/thumbnail.jpg";
+export const demoChannelUrl = "/channel/UCwBjdts6grkMXpVFebk60ZQ";
+export const demoVideoUrl = "/video/YNZ7dhPH6P0";
+export const demoChannelTitle = "Priyanshu [IIIT-G]";
+export const demoVideoTitle =
+  "30 Days in 1 Minute | IIIT GWALIOR | Hostel Life Mini vlog";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
